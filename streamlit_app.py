168d69d18547aa3ec3a92363dd00b398db6a378e
Brain AI <73767162+brainai-hub@users.noreply.github.com>
2024-09-22 22:37:49 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,7 +91,7 @@ if source_radio == "WEBCAM":
     uploaded_image_cv = cv2.cvtColor(numpy.array(uploaded_image), cv2.COLOR_RGB2BGR)
     boxes, resized_image = utils.predict_image(uploaded_image_cv, conf_threshold)
     visualized_image = utils.convert_result_to_image(uploaded_image_cv, resized_image, boxes, conf_labels=False)
-    st.image(visualized_image, channels = "BGR")
+    st.image(visualized_image, channels = "RGB")
 
 clip = mpy.ImageSequenceClip(video_row, fps = fps)
 clip.write_videofile(temp_file_2.name)
